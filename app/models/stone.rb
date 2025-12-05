50779536a85eea3f6d0f012b8304ab99abe23d8f
Enoki181 <wakakusa.owl.113288@gmail.com>
2025-12-05 21:14:49 +0900

    Add:Stone_model and migrate

---
+++ app/models/stone.rb
@@ -0,0 +1,12 @@
+class Stone < ApplicationRecord
+  validates :name_ja, presence: true
+  validates :name_en, presence: true
+  validates :min_mood, presence: true, numericality: {only_integer: true}
+  validates :max_mood, presence: true, numericality: {only_integer: true}
+  # mood_valueは整数だけにマッチさせる(浮動小数点数値にマッチさせない)
+
+  # 気分値から該当する石を検索
+  def self.find_by_mood(mood_value)
+    where('min_mood <= ? AND max_mood >= ?', mood_value, mood_value).first
+  end
+end
