bbcd2882afcb06667344ff1ddf6c8cae141205c2
Enoki181 <wakakusa.owl.113288@gmail.com>
2025-12-07 16:17:50 +0900

    TOPページ実装(機能なし)

--- app/models/stone.rb
+++ app/models/stone.rb
@@ -1,6 +1,7 @@
 class Stone < ApplicationRecord
   validates :name_ja, presence: true
   validates :name_en, presence: true
+  validates :image_path, presence: true
   validates :min_mood, presence: true, numericality: {only_integer: true}
   validates :max_mood, presence: true, numericality: {only_integer: true}
   # mood_valueは整数だけにマッチさせる(浮動小数点数値にマッチさせない)
@@ -9,4 +10,6 @@ class Stone < ApplicationRecord
   def self.find_by_mood(mood_value)
     where('min_mood <= ? AND max_mood >= ?', mood_value, mood_value).first
   end
+
+  mount_uploader :image_path, StoneImageUploader
 end
