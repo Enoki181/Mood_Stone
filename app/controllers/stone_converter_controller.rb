b796a2a86ab9fb17de2c0a57bee444408923d3dc
Enoki181 <wakakusa.owl.113288@gmail.com>
2025-12-15 21:58:11 +0900

    画像追加&シェア機能追加

--- app/controllers/stone_converter_controller.rb
+++ app/controllers/stone_converter_controller.rb
@@ -4,5 +4,8 @@ class StoneConverterController < ApplicationController
   def convert
     mood_value = params[:mood].to_i
     @stone = Stone.find_by_mood(mood_value)
+
+    @post_message = "今の気分に合う石、『#{@stone.name_ja}』をみつけた。"
+    @post_tags = ['MoodStone', @stone.name_en].join(',')
   end
 end
